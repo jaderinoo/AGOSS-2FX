ecc4d9d8ba6df8012e9887105d14c7b012257f78
Jad El-Khatib <jaderinoo@gmail.com>
2020-02-25 00:23:20 -0500

    Fixed cursor logic, added x and y.

--- AGOSS-2FX/src/application/MapCursor.java
+++ AGOSS-2FX/src/application/MapCursor.java
@@ -9,15 +9,14 @@ public class MapCursor {
 	private static Image cursor;
 	private static int cursorPosition;
 	public static double horizontal = 32*(printMap.horizontalSetter/32), vertical = 32*(printMap.verticalSetter/32);
+	private static int cursorX = Adventure.playerListCurrent.get(0).getMapX(), cursorY = Adventure.playerListCurrent.get(0).getMapY();
 	
 	public static Pane init(int cols, int rows)
 	{
 		Rectangle sprite = null;
 		cursor = new Image("application\\tilesets\\cursor\\cursor.gif");
-        
-        
-
-		sprite = new Rectangle(vertical * Adventure.playerListCurrent.get(0).getMapX(), horizontal * Adventure.playerListCurrent.get(0).getMapY(), vertical, horizontal);
+		
+		sprite = new Rectangle(vertical * cursorX, horizontal * cursorY, vertical, horizontal);
 		sprite.setFill(new ImagePattern(cursor));
 		sprite.setId("cursor");
 		
@@ -27,12 +26,14 @@ public class MapCursor {
 		Pane cursorLayer = new Pane();
 		cursorLayer.getChildren().add(sprite);
 		
+		System.out.println("Cursor created.");
 		return cursorLayer;
-		
 	}
 	
 	public static void resetCursor() {
-		System.out.println("Reseting cursor position");
+		System.out.println("Reseting cursor position to: " + Adventure.playerListCurrent.get(0).getMapX() + "," + Adventure.playerListCurrent.get(0).getMapY());
 		printMap.shapes.get(cursorPosition).relocate(vertical * Adventure.playerListCurrent.get(0).getMapX(), horizontal * Adventure.playerListCurrent.get(0).getMapY());
+		cursorX = Adventure.playerListCurrent.get(0).getMapX();
+		cursorY = Adventure.playerListCurrent.get(0).getMapY();
 	}
 }
