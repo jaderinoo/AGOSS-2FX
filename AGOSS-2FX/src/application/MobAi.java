26036b43a6861f19b27f463a256800d63f0c38e6
Jad El-Khatib <jaderinoo@gmail.com>
2020-03-07 23:45:53 -0500

    Setup class for ai movement

---
+++ AGOSS-2FX/src/application/MobAi.java
@@ -0,0 +1,29 @@
+package application;
+
+public class MobAi {
+	public static void moveAi(int type) {
+
+		switch(type) {
+		//Aggressive ai
+		case 0:
+
+			break;
+			//Passive ai
+		case 1:
+
+			break;
+			//Defensive ai
+		case 2:
+
+			break;
+
+			//static ai
+		case 3:
+
+			break;
+
+		default:
+
+		}
+	}
+}
