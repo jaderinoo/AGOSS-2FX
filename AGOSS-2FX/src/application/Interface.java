a356629bf1223eca157b147b5fc8ac78962f36f8
Jad El-Khatib <jaderinoo@gmail.com>
2020-03-16 17:48:52 -0400

    Additional work on bottom bar

--- AGOSS-2FX/src/application/Interface.java
+++ AGOSS-2FX/src/application/Interface.java
@@ -1,16 +1,26 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class Interface {
 	public String time = "";
 	
 	@FXML
-	public TextField timerText;
+	public AnchorPane mainPane;
 
 	@FXML
     public void initialize() {
+		System.out.println(Main.window.getWidth());
+		Rectangle rect = new Rectangle(0, Main.window.getHeight(), Main.window.getWidth(), 150);
+		rect.setFill(Color.BEIGE);
+
+		mainPane.getChildren().add(rect);
 		
     }    
 	
