09c1c6c6eeca6622dae0652955ed41f67437af0c
Jad El-Khatib <jaderinoo@gmail.com>
2020-03-17 02:13:34 -0400

    Reworked into vbox group that gets added as a layer

--- AGOSS-2FX/src/application/Interface.java
+++ AGOSS-2FX/src/application/Interface.java
@@ -1,10 +1,14 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -12,16 +16,23 @@ public class Interface {
 	public String time = "";
 	
 	@FXML
-	public AnchorPane mainPane;
+	public static AnchorPane mainPane;
 
 	@FXML
-    public void initialize() {
-		System.out.println(Main.window.getWidth());
-		Rectangle rect = new Rectangle(0, Main.window.getHeight(), Main.window.getWidth(), 150);
-		rect.setFill(Color.BEIGE);
-
-		mainPane.getChildren().add(rect);
+    public static Group initialize() throws IOException {
 		
+		Group group = new Group();
+
+	    VBox leftBar =  FXMLLoader.load(printMap.class.getResource("scenes\\sidebarUI.fxml"));
+	    group.getChildren().add(leftBar);
+	    
+	    VBox rightBar =  FXMLLoader.load(printMap.class.getResource("scenes\\sidebarUI.fxml"));
+	    group.getChildren().add(rightBar);
+	    
+	    VBox bottomBar =  FXMLLoader.load(printMap.class.getResource("scenes\\sidebarUI.fxml"));
+	    group.getChildren().add(bottomBar);
+		
+		return group;
     }    
 	
 	public void timeUpdater(String timeString) {
