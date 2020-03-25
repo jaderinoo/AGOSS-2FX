923d6743870a6c3b7727c91a66a326e816607169
Jad El-Khatib <jaderinoo@gmail.com>
2020-03-25 05:46:15 -0400

    Waits for all enemy sprites to cycle through movement, uses Thread.sleep

--- AGOSS-2FX/src/application/GameVariables.java
+++ AGOSS-2FX/src/application/GameVariables.java
@@ -24,6 +24,7 @@ public class GameVariables {
 			
 			//Move to enemy turn
 			MobAi.cycleList();
+			
 		}
 	}
 	
--- AGOSS-2FX/src/application/MobAi.java
+++ AGOSS-2FX/src/application/MobAi.java
@@ -4,55 +4,78 @@ import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
 public class MobAi {
+	
 	static int i = 0;
+	static int tempTimer = 0;
 	//Go through the enemy list until all enemies have moved/tried moving.
 	public static void cycleList() {
-		Timer myTimer = new Timer();
-
-		myTimer.scheduleAtFixedRate(new TimerTask(){
+		
+		Timer cycleTimer = new Timer();
+		cycleTimer.scheduleAtFixedRate(new TimerTask(){
+			ArrayList <String> mobMove = new ArrayList<>();
 			@Override
 			public void run() {
-				ArrayList <String> mobMove = new ArrayList<>();
 				char type = mapInitialization.mobList.get(i).getType();
 				
-				//Get the moveset
 				if(type == 'f') {
-					mobMove = moveAi(mapInitialization.mobList.get(i), 0);
+					mobMove = moveAi(mapInitialization.mobList.get(i), 1);
 				}else if(type == 'k') {
 					mobMove = moveAi(mapInitialization.mobList.get(i), 0);
 				}
-
+				
 				//Send the arraylist to the moveSprite method
 				printMap.moveSprite(mapInitialization.mobList.get(i), null, mobMove);
 				
+				try {
+					Thread.sleep(mobMove.size()*250);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				//Increment i and compare to total size
 				i++;
 
 				if(i == mapInitialization.mobList.size()) {
 					i = 0;
-					myTimer.cancel();
-					mobMove.clear();
+					cycleTimer.cancel();
 					GameVariables.roundReset();
+					System.out.println("Ememy phase completed");
 				}
 			}
-		}, 1000, 1000);
+		}, 0, 100);
 	}
 
 	//Call enemy ai type
-	public static ArrayList <String>  moveAi(Mob1 mob, int type) {
+	public static ArrayList <String> moveAi(Mob1 mob, int type) {
 		ArrayList <String> mobeMove = new ArrayList<>();
 		switch(type) {
 		//Aggressive ai
 		case 0:
 			mobeMove.add("up");
-			mobeMove.add("up");
+			mobeMove.add("right");
+			mobeMove.add("right");
+			mobeMove.add("right");
+			mobeMove.add("right");
+			mobeMove.add("right");
+			mobeMove.add("right");
+			mobeMove.add("left");
+			mobeMove.add("left");
+			mobeMove.add("left");
+			mobeMove.add("left");
 			mobeMove.add("left");
 
 			System.out.println(mob.getName());
 			break;
 			//Passive ai
 		case 1:
+			mobeMove.add("right");
+			mobeMove.add("right");
+			mobeMove.add("right");
+			mobeMove.add("right");
+			mobeMove.add("left");
 			System.out.println(mob.getName());
 			break;
 			//Defensive ai
